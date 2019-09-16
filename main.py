549f802118b94964c249e05bc4483d906b07f411
hahnah <superhahnah@gmail.com>
2019-09-16 17:06:48 +0900

    Implement searching places and populartimes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import math
 import populartimes
 import flask
 
@@ -12,10 +13,29 @@ def popular_times(request):
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
     request_json = request.get_json()
+    current_latitude = 35.573016
+    current_longtitude = 139.7174431
+    searching_radius = 500
     api_key = os.environ.get('API_KEY', 'Specified environment variable is not set.')
-    if request.args and 'place_id' in request.args:
-	    return flask.jsonify(populartimes.get_id(api_key, request.args.get('place_id')))
-    elif request_json and 'place_id' in request_json:
-        return flask.jsonify(populartimes.get_id(api_key, request_json['place_id']))
+    place_types = ['cafe']
+    delimiting_points = calulate_delimiliting_points(current_latitude, current_longtitude, searching_radius)
+    delimiting_point1 = delimiting_points[0]
+    delimiting_point2 = delimiting_points[1]
+    number_of_threads = 20
+    should_include_places_even_without_populartimes = False
+    if request.args and 'latitude' in request.args and 'longtitude' in request.args:
+        return flask.jsonify(populartimes.get(api_key, place_types, delimiting_point1, delimiting_point2, number_of_threads, searching_radius, should_include_places_even_without_populartimes))
+    elif request_json and 'latitude' in request_json and 'longtitude' in request_json:
+        return flask.jsonify(populartimes.get(api_key, place_types, delimiting_point1, delimiting_point2, number_of_threads, searching_radius, should_include_places_even_without_populartimes))
     else:
         return flask.jsonify({})
+
+def calulate_delimiliting_points(latitude, longtitude, serching_radius):
+    PI = math.pi
+    EQUATORIAL_RADIUS = 6378136.6
+    POLAR_RADIUS = 6356751.9
+    latitude_difference = (180 * serching_radius) / (PI * POLAR_RADIUS)
+    longtitude_difference = (180 * serching_radius) / (PI * EQUATORIAL_RADIUS * math.cos(latitude))
+    southwest_delimiliting_point = (latitude - latitude_difference, longtitude - longtitude_difference)
+    northeast_delimiliting_point = (latitude + latitude_difference, longtitude + longtitude_difference)
+    return (southwest_delimiliting_point, northeast_delimiliting_point)
