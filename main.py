a79a4fa702da597ba9040b27dee4e64baa89eddf
hahnah <superhahnah@gmail.com>
2019-09-17 00:39:23 +0900

    Format response data to include current pupularity of the places by calculating current datetime. BUT, timezone does't work so far.

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import os
 import math
 import populartimes
 import flask
+from pytz import timezone
+from timezonefinder import TimezoneFinder
+from datetime import datetime, date
 
+timezone_finder = TimezoneFinder(in_memory=True)
 
 def popular_times(request):
     """Responds to any HTTP request.
@@ -35,9 +39,14 @@ def popular_times(request):
     delimiting_point2 = delimiting_points[1]
 
     search_result = populartimes.get(api_key, PLACE_TYPES, delimiting_point1, delimiting_point2, NUMBER_OF_THREADS, SEARCHING_RADIUS, SHOULD_INCLUDE_PLACES_EVEN_WITHOUT_POPULARTIMES)
+    formated_result = format_data(latitude, longtitude, search_result)
     result_json = {
         'status': 'SUCCESS',
-        'search_result': search_result
+        'search_result': formated_result,
+        'datetime': datetime.now(), # DEV LOG
+        # FIXME: timezone does not work
+        'datetime_localized': datetime.now(timezone(timezone_finder.timezone_at(lat=latitude, lng=longtitude))), # DEV LOG
+        'zz_row_result': search_result # DEV LOG
     }
     return flask.jsonify(result_json)
 
@@ -51,3 +60,27 @@ def calulate_delimiliting_points(latitude, longtitude, serching_radius):
     southwest_delimiliting_point = (latitude - latitude_difference, longtitude - longtitude_difference)
     northeast_delimiliting_point = (latitude + latitude_difference, longtitude + longtitude_difference)
     return (southwest_delimiliting_point, northeast_delimiliting_point)
+
+
+def format_data(latitude, longitude, row_data):
+    WEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    timezone_string = timezone_finder.timezone_at(lat=latitude, lng=longitude)
+    # FIXME: timezone does not work
+    current_datetime = datetime.now(timezone(timezone_string))
+    current_date_integer = datetime(current_datetime.year, current_datetime.month, current_datetime.day).weekday()
+    current_hour = current_datetime.hour
+    current_date = WEEK[current_date_integer]
+    return list(map(lambda x: format_place_data(current_date, current_hour, x), row_data))
+
+
+def format_place_data(current_date, current_hour, place_data):
+    todays_populartimes = list(filter(lambda x: x['name'] == current_date, place_data['populartimes']))[0]
+    current_popularity = todays_populartimes['data'][current_hour]
+    return {
+        'address': place_data['address'],
+        'coordinates': place_data['coordinates'],
+        'id': place_data['id'],
+        'name': place_data['name'],
+        'popularity': current_popularity,
+        'rating': place_data['rating']
+    }
